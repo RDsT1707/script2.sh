239f15d996a4ce0bacfeb396c6ec656c1011e82a
rayan <desmetrayan621@gmail.com>
2025-03-07 10:47:36 +0100

    exo4.1

--- exercices3/script3.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-read mkdir (nom)
---
+++ exercices4/script4.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ mkdir -p exo3/public/css/{footer/{styles.css} header/{styles.css} main/{styles.css}} img/{imag1 imag2} js/{main.js} index.html
